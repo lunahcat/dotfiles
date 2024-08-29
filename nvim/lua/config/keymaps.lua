b4bcc0ad18412a26c4e75b264e05e24e3d63ac54
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2024-08-30 03:08:32 +0900

    Add files via upload

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+local keymap = vim.keymap
+
+keymap.set("n", "<C-a>", "ggVG")
+keymap.set("n", "a", "<S-A>")
+
+keymap.set("i", "jj", "<Esc>")
+
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup()
+    end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup()
+    end
+  }
+}
