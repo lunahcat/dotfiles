f5a411cab47fbdd794374360f4aa6d51dc712fd1
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2024-10-24 16:11:33 +0900

    Add files via upload

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,9 +4,3 @@ keymap.set("n", "<C-a>", "ggVG")
 keymap.set("n", "a", "<S-A>")
 
 keymap.set("i", "jj", "<Esc>")
-
-keymap.set("n", "<A-j>", ":m .+1<CR>==")
-keymap.set("n", "<A-k>", ":m .-2<CR>==")
-
-keymap.set("n", "<C-h>", "<C-w>h>")
-keymap.set("n", "<C-l>", "<C-w>l>")
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -11,16 +11,20 @@ return {
 					custom = { "^\\._" },
 				},
 			})
-			vim.keymap.set("n", "<C-j>", "<Cmd>NvimTreeToggle<CR>")
+			local keymap = vim.keymap
+			keymap.set("n", "<C-j>", "<Cmd>NvimTreeToggle<CR>")
+			keymap.set("n", "<C-h>", "<C-w>h")
+			keymap.set("n", "<C-l>", "<C-w>l")
 		end,
 	},
 	{
 		"romgrk/barbar.nvim",
 		config = function()
 			require("barbar").setup()
-			vim.keymap.set("n", "<S-l>", "<Cmd>BufferNext<CR>")
-			vim.keymap.set("n", "<S-h>", "<Cmd>BufferPrevious<CR>")
-			vim.keymap.set("n", "<S-q>", "<Cmd>BufferClose<CR>")
+			local keymap = vim.keymap
+			keymap.set("n", "<S-l>", "<Cmd>BufferNext<CR>")
+			keymap.set("n", "<S-h>", "<Cmd>BufferPrevious<CR>")
+			keymap.set("n", "<S-q>", "<Cmd>BufferClose<CR>")
 		end,
 	},
 	{
